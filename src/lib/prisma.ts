1f3e8a73f5d5443ecaab5e098788d71eddf3bba1
nogamichal1 <michal.noga@hogs.live>
2025-04-25 21:59:35 +0200

    Add files via upload

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,22 @@
 
 import { PrismaClient } from '@prisma/client';
 
+const DATABASE_URL = process.env.DATABASE_URL || 'mysql://root:da2xY2vyCVrxlb2Q@54.38.137.2:3706/kontrahenci_db';
+
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma = global.prisma || new PrismaClient({ log: ['warn', 'error'] });
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['warn', 'error'],
+    datasources: {
+      db: {
+        url: DATABASE_URL,
+      },
+    },
+  });
+
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
