d11518e277b4150f8e7328e621463196bb9c7919
nogamichal1 <michal.noga@hogs.live>
2025-04-25 13:03:35 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   experimental: {
-    appDir: true,
     typedRoutes: true
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
   }
-}
-module.exports = nextConfig
+};
+
+module.exports = nextConfig;
